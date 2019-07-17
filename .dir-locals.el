72ae36e60aec30200d41a60e979d13269980e008
Björn Lindqvist <bjourne@gmail.com>
2019-07-17 19:18:30 +0200

    Indentation settings for Emacs users

---
+++ .dir-locals.el
@@ -0,0 +1,13 @@
+; -*- mode: Lisp -*-
+((c++-mode (c-basic-offset . 2)
+           (c-offsets-alist
+            (access-label . -)
+            (arglist-cont-nonempty . c-lineup-arglist)
+            (defun-block-intro . +)
+            (inclass . +)
+            (innamespace . +)
+            (statement-block-intro . +)
+            (topmost-intro . 0)
+            (topmost-intro-cont . 0)
+            (member-init-intro . +)
+           )))
