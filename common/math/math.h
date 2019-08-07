89727d6c1cc13ac12d28b6a05613374bcc8d9d6e
Björn Lindqvist <bjourne@gmail.com>
2019-08-07 21:05:52 +0200

    orf function

--- common/math/math.h
+++ common/math/math.h
@@ -80,6 +80,9 @@ namespace embree
       _mm_and_ps(_mm_set_ss(x),
                  _mm_castsi128_ps(_mm_set1_epi32(0x80000000))));
   }
+  __forceinline float orf(const float x, const float y) {
+    return _mm_cvtss_f32(_mm_or_ps(_mm_set_ss(x),_mm_set_ss(y)));
+  }
   __forceinline float xorf( const float x, const float y ) {
     return _mm_cvtss_f32(_mm_xor_ps(_mm_set_ss(x),_mm_set_ss(y)));
   }
